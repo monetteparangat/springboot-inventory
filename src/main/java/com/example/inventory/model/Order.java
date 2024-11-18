fd48d99dc485cbc3b8ef04f083b0d21ff48d1c74
MonetteParangat <monette.parangat@ibm.com>
2024-11-19 06:38:12 +0800

    order model commit

---
+++ src/main/java/com/example/inventory/model/Order.java
@@ -0,0 +1,57 @@
+package com.example.inventory.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long orderId;
+
+	@ManyToOne
+	@JoinColumn(name = "customer_id", nullable = false)
+	private Customer customer;
+
+	@Column(nullable = false)
+	private LocalDateTime orderDate;
+
+	@Column(nullable = false)
+	private String orderStatus;
+
+	private String shippingMethod;
+
+	@Column(nullable = false)
+	private Double totalAmount;
+
+	@Column(nullable = false)
+	private String paymentStatus; // e.g., "paid", "unpaid", "partially paid"
+
+	private String shippingAddress; // If different from billing address
+
+	private String invoiceNumber;
+
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems; // List of items in the order
+
+	@PrePersist
+	protected void onCreate() {
+		orderDate = LocalDateTime.now();
+	}
+
+}
