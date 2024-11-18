c9c146d34e0e8c4703f69b740698b4124e360a7e
MonetteParangat <monette.parangat@ibm.com>
2024-11-19 06:49:33 +0800

    orderitem model commit

--- src/main/java/com/example/inventory/model/OrderItem.java
+++ src/main/java/com/example/inventory/model/OrderItem.java
@@ -1,5 +1,46 @@
 package com.example.inventory.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class OrderItem {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double pricePerUnit;
+
+    @Column(nullable = false)
+    private Double totalPrice;
+
+    // PrePersist method to calculate total price based on quantity and unit price
+    @PrePersist
+    protected void onCreate() {
+        totalPrice = quantity * pricePerUnit;
+    }
 }
