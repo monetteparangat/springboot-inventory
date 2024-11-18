2cd79831f6b6d7006126b926b0e3ad11176299b4
MonetteParangat <monette.parangat@ibm.com>
2024-11-19 06:55:50 +0800

    customer model commit

--- src/main/java/com/example/inventory/model/Customer.java
+++ src/main/java/com/example/inventory/model/Customer.java
@@ -1,5 +1,39 @@
 package com.example.inventory.model;
 
-public class Customer {
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customerId;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	private String phoneNumber;
+
+	private String billingAddress;
+
+	private String shippingAddress;
+
+	private String accountType; // e.g., "regular", "wholesale"
+
+	@OneToMany(mappedBy = "customer")
+	private List<Order> orders; // List of orders placed by the customer
 }
