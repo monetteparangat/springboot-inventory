88417a1e1cee116a4e537f4e4405ff0855e985c1
MonetteParangat <monette.parangat@ibm.com>
2024-11-19 20:04:42 +0800

    StockMovementService.java - commit CRUD

---
+++ src/main/java/com/example/inventory/repository/StockMovementRepository.java
@@ -0,0 +1,24 @@
+package com.example.inventory.repository;
+
+import java.util.List;
+
+import com.example.inventory.model.StockMovement;
+
+public class StockMovementRepository {
+
+	public List<StockMovement> findAll() {
+		return null;
+	}
+
+	public StockMovement findById(Long id) {
+		return null;
+	}
+
+	public StockMovement save(StockMovement stockMovemet) {
+		return null;
+	}
+
+	public void delete(Long id) {
+		
+	}
+}
---
+++ src/main/java/com/example/inventory/service/StockMovementService.java
@@ -0,0 +1,48 @@
+package com.example.inventory.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.inventory.model.StockMovement;
+import com.example.inventory.repository.StockMovementRepository;
+
+@Service
+public class StockMovementService {
+
+	@Autowired
+	private StockMovementRepository stockMovementRepository;
+
+	public List<StockMovement> getAllStockMovements() {
+		return stockMovementRepository.findAll();
+	}
+
+	public StockMovement getStockMovementById(Long id) {
+		return stockMovementRepository.findById(id);
+	}
+
+	public StockMovement addStockMovement(StockMovement stockMovement) {
+		return stockMovementRepository.save(stockMovement);
+	}
+
+	public StockMovement updateStockMovement(Long id, StockMovement updatedStockMovement) {
+		StockMovement existingStockMovement = stockMovementRepository.findById(id);
+
+		// validate of existing
+		existingStockMovement.setProduct(updatedStockMovement.getProduct());
+		existingStockMovement.setTransactionType(updatedStockMovement.getTransactionType());
+		existingStockMovement.setQuantityChanged(updatedStockMovement.getQuantityChanged());
+		existingStockMovement.setDate(updatedStockMovement.getDate());
+		existingStockMovement.setPrice(updatedStockMovement.getPrice());
+		existingStockMovement.setReason(updatedStockMovement.getReason());
+		existingStockMovement.setReferenceNumber(updatedStockMovement.getReferenceNumber());
+
+		return stockMovementRepository.save(existingStockMovement);
+	}
+
+	public void deleteStockMovementById(Long id) {
+		stockMovementRepository.delete(id);
+	}
+
+}
